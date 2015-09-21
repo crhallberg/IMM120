5df3e881a20ed9a68b42390e725c472d554c6644
soacuser <soacuser@aimm202-teach.lions.tcnj.edu>
2015-09-21 17:51:56 -0400

    Image added.

--- today/rockpaperscissors.js
+++ today/rockpaperscissors.js
@@ -1,45 +1,49 @@
 // Variables
+var rock;
 
 // Runs first and once
 function setup() {
-  createCanvas(800, 400); // size
-  background(255);
-  fill(0);
-  textSize(45);
+    createCanvas(800, 400); // size
+    background(255);
+    fill(0);
+    textSize(45);
+
+    rock = loadImage('rock.png');
 }
 
 // Loops forever
 function draw() {
-  // We're not looping any drawing
+    // We're not looping any drawing
+    image(rock, 0, 0);
 }
 
 function keyPressed() {
-  // RESET
-  if (key === ' ') {
-    background(255);
-  }
-  // LEFT SIDE
-  textAlign(LEFT);
-  var padding = 10;
-  if (key === 'A') {
-    text('ROCK', padding, 150);
-  }
-  if (key === 'S') {
-    text('PAPER', padding, 200);
-  }
-  if (key === 'D') {
-    text('SCISSORS', padding, 250);
-  }
-  // RIGHT SIDE
-  textAlign(RIGHT);
-  var padding = 10;
-  if (key === 'J') {
-    text('ROCK', width-padding, 150);
-  }
-  if (key === 'K') {
-    text('PAPER', width-padding, 200);
-  }
-  if (key === 'L') {
-    text('SCISSORS', width-padding, 250);
-  }
+    // RESET
+    if (key === ' ') {
+        background(255);
+    }
+    // LEFT SIDE
+    textAlign(LEFT);
+    var padding = 10;
+    if (key === 'A') {
+        text('ROCK', padding, 150);
+    }
+    if (key === 'S') {
+        text('PAPER', padding, 200);
+    }
+    if (key === 'D') {
+        text('SCISSORS', padding, 250);
+    }
+    // RIGHT SIDE
+    textAlign(RIGHT);
+    var padding = 10;
+    if (key === 'J') {
+        text('ROCK', width - padding, 150);
+    }
+    if (key === 'K') {
+        text('PAPER', width - padding, 200);
+    }
+    if (key === 'L') {
+        text('SCISSORS', width - padding, 250);
+    }
 }
