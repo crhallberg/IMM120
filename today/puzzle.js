e84f2b6b46ed85c64e919dc584875b99385b8cd2
Chris Hallberg <crhallberg@gmail.com>
2015-09-28 16:19:50 -0400

    4 space conversion

--- today/puzzle.js
+++ today/puzzle.js
@@ -4,26 +4,26 @@ var moonHeight = 250;
 var angle = 0;
 
 function setup() {
-  createCanvas(600, 600);
-  moon = loadImage('newmoon.png');
+    createCanvas(600, 600);
+    moon = loadImage('newmoon.png');
 }
 
 function draw() {
-  drawMoon();
+    drawMoon();
 
-  background(0);
+    background(0);
 
-  if (isMousePressed) {
-    angle == angle + 2;
-  }
+    if (isMousePressed) {
+        angle == angle + 2;
+    }
 }
 
 function drawMoon() {
-  push();
+    push();
 
-  rotate(radians(angle));
-  translate(mouseX, mouseY);
-  image(moon, -moonWidth/2, -moonHeight/2);
+    rotate(radians(angle));
+    translate(mouseX, mouseY);
+    image(moon, -moonWidth / 2, -moonHeight / 2);
 
-  pop();
+    pop();
 }
