d2f54240d9b1376f5517f7e5be98887777169d19
Chris Hallberg <crhallberg@gmail.com>
2015-10-05 16:36:23 -0400

    Today's starting point and puzzle.

--- today/sketch.js
+++ today/sketch.js
@@ -2,11 +2,17 @@
 
 // Runs first and once
 function setup() {
-  createCanvas(640, 480); // size
+    createCanvas(700, 500); // size
 }
 
 // Loops forever
 function draw() {
-  background(102);
-  // and other magic
+    background(102);
+
+    ellipse(100, 250, 50, 50);
+    ellipse(200, 250, 50, 50);
+    ellipse(300, 250, 50, 50);
+    ellipse(400, 250, 50, 50);
+    ellipse(500, 250, 50, 50);
+    ellipse(600, 250, 50, 50);
 }
