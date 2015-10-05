c2cc53169363ab4fc01c0e034f04076a5a25a368
soacuser <soacuser@aimm202-teach.lions.tcnj.edu>
2015-10-05 19:21:11 -0400

    Comments.

--- today/arrays.js
+++ today/arrays.js
@@ -9,6 +9,8 @@ function setup() {
     
     for(var i=0; i<100; i++) {
         circleSizes[i] = random(10, 150);
+        
+        println(i, i % 7);
     }
 }
 
@@ -18,10 +20,14 @@ function draw() {
     stroke(255);
     
     for (var count = 0; count < circleSizes.length; count++) {
-        var x = count * 50 + 75;
-        fill(colors[count % 7]);
+        var x = count * 50 + 75; // position formula
+        fill(colors[count % 7]); // MODULO
         ellipse(x, 240, circleSizes[count], circleSizes[count]);
-
-        println(count);
     }
 }
+
+
+
+
+
+
