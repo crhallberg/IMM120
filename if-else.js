94b3ce141c6351280a1606e409004d7a1ca6a9ab
Chris Hallberg <crhallberg@gmail.com>
2015-11-02 13:51:25 -0500

    if-else challenge
    Using only ifs and if-elses, print out the following statements when appropriate:
     - All are true
     - All are false
     - A and C, but not B
     - B and C, but not A
     - Only A
    
    Extra challenge: no && or ||, no ! (changes true to false and vice versa)

---
+++ if-else.js
@@ -0,0 +1,16 @@
+// Variables
+var A = true;
+var B = false;
+var C = true;
+
+// Runs first and once
+function setup() {
+  createCanvas(700, 100); // size
+  textSize(26);
+}
+
+// Loops forever
+function draw() {
+  background(255);
+  text('Surround me with if-elses!', 10, 40);
+}
