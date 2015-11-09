d698a61c4ebae4bb6b7b110ef429b98cb49c145c
soacuser <soacuser@aimm202-teach.lions.tcnj.edu>
2015-11-09 18:20:54 -0500

    CLick to add objects.

--- Nov9/objects.js
+++ Nov9/objects.js
@@ -1,11 +1,5 @@
 // Variables
 var objects = [];
-var ball = {
-    x: 400,
-    y: 200,
-    xspeed: 2,
-    yspeed: 3
-};
 
 var gravity = .2;
 
@@ -18,37 +12,31 @@ function setup() {
 function draw() {
     background(200);
     
-    ellipse(ball.x, ball.y, 100, 100);
-    
-    ball.x += ball.xspeed;
-    ball.y += ball.yspeed;
-    ball.yspeed += gravity;
-    
-    if (ball.x > width || ball.x < 0) {
-        ball.xspeed = -ball.xspeed;
-    }
-    // Bounce
-    if (ball.y > height || ball.y < 0) {
-        ball.yspeed = -.8 * ball.yspeed;
+    for(var i=0; i<objects.length; i++) {
+        var ball = objects[i];
+        
+        ellipse(ball.x, ball.y, 100, 100);
+
+        ball.x += ball.xspeed;
+        ball.y += ball.yspeed;
+        ball.yspeed += gravity;
+
+        if (ball.x > width || ball.x < 0) {
+            ball.xspeed = -ball.xspeed;
+        }
+        // Bounce
+        if (ball.y > height || ball.y < 0) {
+            ball.yspeed = -.8 * ball.yspeed;
+        }
     }
 }
 
-function mousePressed() {
-    ball.x = mouseX;
-    ball.y = mouseY;
-}
-
-function keyPressed() {
-    if (keyCode == LEFT_ARROW) {
-        hAcc = -.1;
-    }
-    if (keyCode == RIGHT_ARROW) {
-        hAcc = .1;
-    }
-    if (keyCode == UP_ARROW) {
-        vAcc = -.1;
-    }
-    if (keyCode == DOWN_ARROW) {
-        vAcc = .1;
-    }
+function mousePressed() {    
+    var circle = {
+        x: mouseX,
+        y: mouseY,
+        xspeed: 2,
+        yspeed: 3
+    };
+    objects.push(circle);
 }
