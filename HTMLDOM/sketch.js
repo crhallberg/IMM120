a93567e5e290c1633409aaa473a48294fb7d7f1d
soacuser <soacuser@aimm202-teach.lions.tcnj.edu>
2015-11-23 18:47:58 -0500

    Making links and stuff

--- HTMLDOM/sketch.js
+++ HTMLDOM/sketch.js
@@ -3,6 +3,17 @@ function setup() {
     canvas.parent('#sketch');
     
     var third = select('#third');
+    third.html('THIS CHANGES <u>EVERYTHING</u>!!');
+    third.style('background', 'palegreen');
+    third.style('padding', '16px');
+    
+    var boldElements = selectAll('b');
+    for (var i=0; i<boldElements.length; i++) {
+        boldElements[i].html('<img src="http://cliparts.co/cliparts/6iy/XxE/6iyXxERAT.jpg"/>')
+    }
+    
+    var link = createA('#', 'CLICK ME FOR FUN');
+    select('#sketch').child(link);
 }
 
 function draw() {
